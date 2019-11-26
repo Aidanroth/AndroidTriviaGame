1c1f2fb23b753ea75781a9b0083776bc37dc5fdc
Aidan Roth <AidanRoth@gmail.com>
2019-11-26 15:36:30 -0800

    Added ResultsActivity which QuestionActivity will navigate to after user clicks 'next' button last question

---
+++ app/src/main/java/com/example/triviagame/ResultsActivity.java
@@ -0,0 +1,36 @@
+package com.example.triviagame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+public class ResultsActivity extends AppCompatActivity {
+
+    //Button menuButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        setContentView(R.layout.activity_results);
+        TextView textView = (TextView) findViewById(R.id.player_score);
+        textView.setText("player_score");   // will need to get score variable from QuestionActivity to display here.
+
+        //menuButton = findViewById(R.id.menuButton);
+
+    }
+
+
+    public void onClickMenu(View view) {
+        Intent menuIntent = new Intent(this, MainActivity.class);
+        startActivity(menuIntent);
+    }
+
+
+}
